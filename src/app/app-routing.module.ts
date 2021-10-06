7777fa2bd1b34ee9901c749e525a1012fcd39c25
KyleMaxwell1224 <kyleman1224@gmail.com>
2021-10-06 19:52:01 -0400

    Week 4 readded

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,14 +11,14 @@ const routes: Routes = [
   { path: 'WeekOne', component: WeekOneComponent },
   { path: 'WeekTwo', component: WeekTwoComponent},
   { path: 'WeekThree', component: WeekThreeComponent},
-  //{ path: 'WeekFour', component: WeekFourComponent},
+  { path: 'WeekFour', component: WeekFourComponent},
   { path: 'WeekFive', component: WeekFiveComponent}
 ];
 const displayRoutes = [
   { weekNum: 1, name: 'Week One', path: 'WeekOne' },
   { weekNum: 2, name: 'Week Two', path: 'WeekTwo'},
   { weekNum: 3, name: 'Week Three', path: 'WeekThree'},
-  //{ weekNum: 4, name: 'Week Four', path: 'WeekFour'},
+  { weekNum: 4, name: 'Week Four', path: 'WeekFour'},
   { weekNum: 5, name: 'Week Five', path: 'WeekFive'},
 
 ];
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'HAA0125 - Kyle Maxwell';
+  title = 'Historical Artifacts - Kyle Maxwell';
 }
